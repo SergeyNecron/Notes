287511907e41d2d87aeb395cc367bbecf059450c
SergeyMuratkin <SergeyNecron@yandex.ru>
2020-05-10 20:51:17 +0500

    fix error: npm WARN saveError ENOENT: no such file or directory, open '/home/notes/package.json'

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 file=./node_modules/@vaadin/flow-frontend/gridConnector.js
-if [ -e "$file" ]
-then
-        java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Xmx256m -Xss512k -XX:MetaspaceSize=100m -jar $file
+if [ -e "$file" ]; then
+  java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Xmx256m -Xss512k -XX:MetaspaceSize=100m -jar $file
 else
-        gradle bootRun
+  gradle bootRun
 fi
