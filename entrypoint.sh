b19f60f4402d7a9f6d92171fddbbcf7753223ded
SergeyMuratkin <SergeyNecron@yandex.ru>
2020-05-12 08:50:22 +0500

    Fix error: code=H10 desc="App crashed" method=GET path="/"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Xmx256m -Xss512k -XX:MetaspaceSize=100m -jar ./build/libs/fine-note.jar
-
+java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Xmx126m -Xss512k -XX:MetaspaceSize=100m -jar ./build/libs/fine-note.jar
